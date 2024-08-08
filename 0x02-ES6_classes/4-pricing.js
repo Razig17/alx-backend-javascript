c7ca9a0f25e013c08b287837de8c66adef0aa05c
Razig17 <abdelrazig.sharif1@gmail.com>
2024-08-08 23:38:49 +0300

    Task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,40 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw TypeError('Amount must be a number');
+    }
+    this._amount = amount;
+    if (!(currency instanceof Currency)) {
+      throw TypeError('Currency must be a Currency');
+    }
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount !== 'number') {
+      throw TypeError('Amount must be a number');
+    }
+    this._amount = newAmount;
+  }
+
+  set currency(newCurrency) {
+    if (!(newCurrency instanceof Currency)) {
+      throw TypeError('Currency must be a Currency');
+    }
+    this._currency = newCurrency;
+  }
+}
