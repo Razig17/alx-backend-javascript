fb115038d9168cd44e80ee7018478746bfcde4ef
Razig17 <abdelrazig.sharif1@gmail.com>
2024-08-12 21:58:44 +0300

    Task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,7 @@
 export default function createInt8TypedArray(length, postion, value) {
   const buffer = new ArrayBuffer(length);
   const int8 = new Int8Array(buffer);
-  if (postion <= 0 || postion > length) {
+  if (postion <= 0 || postion > length - 1) {
     throw Error('Position outside the range');
   }
   int8[postion] = value;
