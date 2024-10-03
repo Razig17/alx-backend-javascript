64d008505eb1806446344f769ecb56d42de6062b
Razig17 <abdelrazig.sharif1@gmail.com>
2024-10-04 00:53:16 +0300

    Remove the comments

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -5,12 +5,11 @@ const sendPaymentRequestToApi = require('./4-payment');
 describe('sendPaymentRequestToApi', function() {
   let spy;
 
-  // Before each test, spy on console.log
+
   beforeEach(function() {
     spy = sinon.spy(console, 'log');
   });
 
-  // After each test, restore the original console.log
   afterEach(function() {
     spy.restore();
   });
@@ -18,20 +17,17 @@ describe('sendPaymentRequestToApi', function() {
   it('should call sendPaymentRequestToApi with 100 and 20, and log the correct message', function() {
     sendPaymentRequestToApi(100, 20);
 
-    // Verify console.log was called with the correct message
     expect(spy.calledOnceWithExactly('The total is: 120')).to.be.true;
 
-    // Verify console.log was called once
     expect(spy.calledOnce).to.be.true;
   });
 
   it('should call sendPaymentRequestToApi with 10 and 10, and log the correct message', function() {
     sendPaymentRequestToApi(10, 10);
 
-    // Verify console.log was called with the correct message
     expect(spy.calledOnceWithExactly('The total is: 20')).to.be.true;
 
-    // Verify console.log was called once
+
     expect(spy.calledOnce).to.be.true;
   });
 });
