2c7c76e16603f24207bb2181976296fe3c5e3754
Razig17 <abdelrazig.sharif1@gmail.com>
2024-10-04 00:33:40 +0300

    Remove the comments

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -5,22 +5,16 @@ const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', function() {
   it('should call Utils.calculateNumber with SUM, 100, 20 and log the correct message', function() {
-    // Stub Utils.calculateNumber to always return 10
     const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
 
-    // Spy on console.log
     const spy = sinon.spy(console, 'log');
 
-    // Call the function
     sendPaymentRequestToApi(100, 20);
 
-    // Verify that Utils.calculateNumber was called with the correct arguments
     expect(stub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
 
-    // Verify that console.log was called with the correct message
     expect(spy.calledOnceWithExactly('The total is: 10')).to.be.true;
 
-    // Restore the stub and spy
     stub.restore();
     spy.restore();
   });
